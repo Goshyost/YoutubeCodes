7bdfa77c6f12fd1b9e0c55f2c8a4c17e57a335a9
Goshyost <84984866+Goshyost@users.noreply.github.com>
2024-09-24 00:51:51 -0700

    Create setprefix.js
    Setprefix command, if you are one of those who still uses prefixes, this is for you, i love you

---
+++ setprefix.js
@@ -0,0 +1,58 @@
+/*
+
+  This command uses the Database Manager: MegaDB (npm install megadb)
+  I dont even know if someone still uses the prefix commands
+  If you are one of those, please start considering Slash Commands, those are much easier, trust me
+
+  Anyways, here is the code
+  Hey, those who still uses prefix commands, this is for you <3
+  
+*/
+
+const Discord = require("discord.js")
+const db = require("megadb")
+const setprefix = new db.crearDB("setprefix")
+
+module.exports = {
+    name: "setprefix",
+    alias: [],
+
+    execute(client, message, args){
+
+        if(args[0]) return message.channel.send({ content: "Debes especificar el prefijo" })
+
+        if(setprefix.establecer(message.guild.id, args[0]))
+        if(!message.member.permissions.has("Administrator")) return message.channel.send({ content: "" })
+      
+    }
+}
+
+// File: setprefix ------------------------------------------------------------------------------------------------------
+
+{
+  "GUILDID": "!"
+}
+
+// File: Index.js -------------------------------------------------------------------------------------------------------
+
+const db = require("megadb")
+const setprefix = new db.crearDB("setprefix")
+
+client.on("messageCreate", async message => {
+  
+let prefix;
+  
+if(setprefix.tiene(message.guild.id)){
+  let prefix = await setprefix.obtener(message.guild.id)
+} else {
+  prefix = "x."
+}
+  
+if(message.author.bot) return;
+if (message.content.startsWith(prefix)) return;
+
+  // You can see the rest of this part of the code in the command handler video...
+
+})
+
+// ----------------------------------------------------------------------------------------------------------------------
